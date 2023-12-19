bd1c5237705436a9927d8aaf1e42dcbaec3dcb0d
Joseph Livesey <jlivesey@bmt.world>
2023-12-19 15:00:24 -0500

    fix: add basic OpenAI GPT-4 chat completion functionality

---
+++ parsey.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+stream = client.chat.completions.create(
+    model="gpt-4",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    print(chunk.choices[0].delta.content or "", end="")
