efea3e13b91285ade503f0e270215fded039e048
Joseph Livesey <jlivesey@bmt.world>
2023-12-20 11:17:13 -0500

    feat(parsey-ctl): add support for extracting pdf data
    test(pdf): add test pdf file
    
    fix(build.rs): remove superfluous comments

--- parsey-ctl/build.rs
+++ parsey-ctl/build.rs
@@ -2,8 +2,8 @@ fn main() {
     tonic_build::configure()
         .build_server(true)
         .compile(
-            &["message.proto"], // Add the path to your Protobuf file(s)
-            &["proto/"], // Set the Protobuf includes directory
+            &["message.proto"],
+            &["proto/"],
         )
         .unwrap();
 }
