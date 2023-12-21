36cf79bd8bac0fd73231f40110c3cd87b5d802e7
Joseph Livesey <jlivesey@bmt.world>
2023-12-21 11:04:50 -0500

    refactor(parsey): refactor python code for improved readbility

--- parsey/parsey.py
+++ parsey/parsey.py
@@ -8,27 +8,43 @@ from concurrent.futures import ThreadPoolExecutor  # Import ThreadPoolExecutor f
 from io import BytesIO
 from openai import OpenAI
 
-def show_json(obj):
-    print(json.loads(obj.model_dump_json()))
+class Utils:
+    @staticmethod
+    def show_json(obj):
+        print(json.loads(obj.model_dump_json()))
 
-def create_pdf_from_bytes(pdf_bytes):
-    pdf_writer = PyPDF2.PdfWriter()
-    buffer = BytesIO(pdf_bytes)
-    pdf_reader = PyPDF2.PdfReader(buffer)
-    for page_num in range(len(pdf_reader.pages)):
-        pdf_writer.add_page(pdf_reader.pages[page_num])
-    new_pdf_buffer = BytesIO()
-    pdf_writer.write(new_pdf_buffer)
-    new_pdf_buffer.seek(0)
-    with open('file.pdf', 'wb') as new_pdf_file:
-        new_pdf_file.write(new_pdf_buffer.read())
+    @staticmethod
+    def create_pdf_from_bytes(pdf_bytes):
+        pdf_writer = PyPDF2.PdfWriter()
+        buffer = BytesIO(pdf_bytes)
+        pdf_reader = PyPDF2.PdfReader(buffer)
+        for page_num in range(len(pdf_reader.pages)):
+            pdf_writer.add_page(pdf_reader.pages[page_num])
+        new_pdf_buffer = BytesIO()
+        pdf_writer.write(new_pdf_buffer)
+        new_pdf_buffer.seek(0)
+        with open('file.pdf', 'wb') as new_pdf_file:
+            new_pdf_file.write(new_pdf_buffer.read())
 
 class MessageServiceServicer(message_pb2_grpc.MessageServiceServicer):
     def ProcessText(self, request, context):
         pdf_bytes = request.pdf_bytes
-        create_pdf_from_bytes(pdf_bytes)
-
+        Utils.create_pdf_from_bytes(pdf_bytes)
+        
         client = OpenAI()
+        
+        assistant = self.setup_openai_assistant(client)
+        file = self.create_assistant_file(client)
+        self.update_assistant_with_file(client, assistant, file)
+        thread = self.create_assistant_thread(client)
+        message = self.create_assistant_message(client, thread)
+        run = self.create_assistant_run(client, thread, assistant)
+        self.wait_for_run_completion(client, thread, run)
+        response_text = self.get_assistant_response(client, thread)
+
+        return message_pb2.TextResponse(processed_content=json.dumps(response_text))
+    
+    def setup_openai_assistant(self, client):
         assistant = client.beta.assistants.create(
             name="PDF Text Content Tabular Data Parser",
             instructions="""You specialize in parsing tabular data from pdf file content.
@@ -44,46 +60,61 @@ class MessageServiceServicer(message_pb2_grpc.MessageServiceServicer):
             tools=[{"type": "retrieval"}],
             model="gpt-4-1106-preview"
         )
-        PDF_FILE_PARSER = assistant.id
+        
+        print()
         print("ASSISTANT:")
-        show_json(assistant)
-
+        Utils.show_json(assistant)
+        
+        return assistant
+    
+    def create_assistant_file(self, client):
         file = client.files.create(
             file=open("file.pdf", "rb"),
             purpose='assistants'
         )
-
+        return file
+    
+    def update_assistant_with_file(self, client, assistant, file):
         assistant = client.beta.assistants.update(
-            PDF_FILE_PARSER,
+            assistant.id,
             tools=[{"type": "code_interpreter"}, {"type": "retrieval"}],
             file_ids=[file.id],
         )
+        
+        print()
         print("UPDATED ASSISTANT WITH FILE:")
-        show_json(assistant)
+        Utils.show_json(assistant)
 
+    def create_assistant_thread(self, client):
         thread = client.beta.threads.create()
+        
+        print()
         print("THREAD:")
-        show_json(thread)
-
+        Utils.show_json(thread)
+        
+        return thread
+    
+    def create_assistant_message(self, client, thread):
         message = client.beta.threads.messages.create(
             thread_id=thread.id,
             role="user",
             content="Please perform your task on this PDF file."
         )
+        print()
         print("MESSAGE:")
-        show_json(message)
+        Utils.show_json(message)
+        
+        return message
 
+    def create_assistant_run(self, client, thread, assistant):
         run = client.beta.threads.runs.create(
             thread_id=thread.id,
             assistant_id=assistant.id,
         )
-        show_json(run)
-
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id
-        )
-
+        return run
+    
+    def wait_for_run_completion(self, client, thread, run):
+        print()
         print("WAITING FOR RUN TO COMPLETE...")
         while run.status == "queued" or run.status == "in_progress":
             run = client.beta.threads.runs.retrieve(
@@ -91,20 +122,16 @@ class MessageServiceServicer(message_pb2_grpc.MessageServiceServicer):
                 run_id=run.id,
             )
             time.sleep(0.5)
-        print("RUN:")
-        show_json(run)
 
+    def get_assistant_response(self, client, thread):
         messages = client.beta.threads.messages.list(thread_id=thread.id)
+        
+        print()
         print("MESSAGES:")
-        show_json(messages)
-
-        response_text = []
-        for msg in messages:
-            content_value = msg.content[0].text.value
-            response_text.append(content_value)
-            break
-
-        return message_pb2.TextResponse(processed_content=json.dumps(response_text))
+        Utils.show_json(messages)
+        
+        response_text = [msg.content[0].text.value for msg in messages if msg.content]
+        return response_text[0] if response_text else None
 
 def serve():
     print("Starting 'parsey' server. Listening on port 50051.")
